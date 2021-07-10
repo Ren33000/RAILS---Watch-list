f6ff73a4956cbc9d09ea8bbc6a212e7326f5c7ca
Mylene Pont <pont.mylene@gmail.com>
2021-07-10 15:12:23 +0900

    seeding done and checked

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,20 +3,13 @@ class ListsController < ApplicationController
         @lists = List.all
     end
 
-  def show
-		@restaurant = Restaurant.find(params[:id])
-  end
+    def show
+	    @list = List.find(params[:id])
+     end
 
-  def new 
-    @restaurant = Restaurant.new
-  end
-		#form to send = authenticity token (need to add hidden fiel tag in the form.html BUT automatic in Rails
-
-
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant) #using the strong params
-	  #don't need to recreate and update the form, use form_for in the html
-  end
+    private
+    def list_params 
+        params.require(:list).permit(:name)
+    end 
 end
+
