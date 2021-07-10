8206ac9622e6ff7488f37d4fe96ba85102f6caa7
Mylene Pont <pont.mylene@gmail.com>
2021-07-10 17:06:28 +0900

    end of the day, has to finish

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,15 @@
 class BookmarksController < ApplicationController
     def new 
         @bookmark = Bookmark.new
+        @list = List.find(params[:list_id])
     end
 
     def create
          @bookmark = Bookmark.new(bookmark_params)
+         @list = List.find(params[:list_id])
+         @bookmark.list = @list
         if @bookmark.save
-            redirect_to bookmark_path(@bookmark)
+            redirect_to list_path(@list)
         else
             render 'new'
         end
@@ -16,4 +19,8 @@ class BookmarksController < ApplicationController
         @bookmark.destroy
         redirect_to bookmarks_path
      end
+
+    def bookmark_params 
+        params.require(:bookmark).permit(:comment, :movie_id)
+    end 
 end
